9aad111601a2a1b15cfe4d697be9f850a61fd339
srividyaghanta534 <srividyaghanta99@gmail.com>
2021-04-17 15:25:32 +0530

    main

---
+++ AverageMarksOfStudent.py
@@ -0,0 +1,9 @@
+average = lambda name: (sum(student_marks[name]) / 3)
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+print("{0:.2f}".format(average(query_name)))
