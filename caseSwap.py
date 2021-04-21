dbe21a5a9cd4a2b162a8a86683631f6aa13e2ac4
srividyaghanta534 <srividyaghanta99@gmail.com>
2021-04-21 11:05:33 +0530

    main

--- caseSwap.py
+++ caseSwap.py
@@ -4,12 +4,14 @@ lo = 0
 for i in string:
     if i.isupper():
         up += 1
+        #print("IN upper")
     else:
         lo += 1
+       # print("In lower")
 if up > lo:
     string = ''.join(string)
-    string.upper()
+    string = string.upper()
 else:
     string = ''.join(string)
-    string.lower()
+    string = string.lower()
 print(string)
